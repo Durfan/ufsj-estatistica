7618f2ea44838793aaf748f261d8f4a5cd1745bf
Durfan <cecilio@aluno.ufsj.edu.br>
2021-01-28 17:35:31 -0300

    sample simple

--- A1.R
+++ A1.R
@@ -1,12 +1,9 @@
-# Usuário informa o tamanho da população
-populacao <- readline(prompt="Tamanho da população: ")
+rm(list=ls())
+N <- readline(prompt="Tamanho da população: ")
+N <- as.numeric(unlist(N))
+n <- as.integer(readline(prompt="Tamanho da amostra: "))
+n <- as.numeric(unlist(n))
 
-# Usuário informa o tamanho da amostra
-amostra <- readline(prompt="Tamanho da amostra: ")
-
-# Usuário informa o plano de amostragem
-# (aleatória simples, sistemática ou estratificada)
-
-# Se estratificada, usuário informa a variável estratificadora
-
-# Retorna os elementos da população a serem incluídos na amostra
+?sample
+k = sample(N, n, replace = FALSE)
+k
