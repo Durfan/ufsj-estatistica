5c14f799f4ba3ac8f3c418886c00cdb50457b876
Durfan <cecilio@aluno.ufsj.edu.br>
2021-01-28 11:42:54 -0300

    user input

--- A1.R
+++ A1.R
@@ -1,6 +1,8 @@
 # Usuário informa o tamanho da população
+pop <- readline(prompt="Tamanho da população: ")
 
 # Usuário informa o tamanho da amostra
+ams <- readline(prompt="Tamanho da amostra: ")
 
 # Usuário informa o plano de amostragem
 # (aleatória simples, sistemática ou estratificada)
