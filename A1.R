c4757ef27f04430ce4d4c9be3c7583205dd5748e
Durfan <cecilio@aluno.ufsj.edu.br>
2021-01-28 11:36:17 -0300

    comments

---
+++ A1.R
@@ -0,0 +1,10 @@
+# Usuário informa o tamanho da população
+
+# Usuário informa o tamanho da amostra
+
+# Usuário informa o plano de amostragem
+# (aleatória simples, sistemática ou estratificada)
+
+# Se estratificada, usuário informa a variável estratificadora
+
+# Retorna os elementos da população a serem incluídos na amostra
