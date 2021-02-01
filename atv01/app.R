8516a6d29248d0feceabddfbb7eefb6611c97718
Durfan <cecilio@aluno.ufsj.edu.br>
2021-02-01 20:22:58 -0300

    foos

--- atv01/app.R
+++ atv01/app.R
@@ -1,3 +1,32 @@
+simples <- function(N, n) {
+  header <- "<h3>Amostragem Aleatória Simples</h3>"
+  amostra <- sample(N, n, replace = F)
+  amostra <- sort(amostra)
+  HTML(header, toString(amostra))
+}
+
+sistematica <- function(N, n) {
+  header <- "<h3>Amostragem Sistemática</h3>"
+  k <- round(N/n)
+  amostra <- sample(1:k,1)
+  amostra <- amostra + (0:(n-1))*k
+  amostra <- sort(amostra)
+  HTML(header, toString(amostra))
+}
+
+estratificada <- function(N, n, var) {
+  Na <- N - var
+  na <- round((n * Na) / N)
+  nb <- round((n * var) / N)
+  amostra_A <- sort(sample(1:Na, na, replace = F))
+  amostra_B <- sort(sample(Na:N, nb, replace = F))
+  header <- "<h3>Amostragem Estratificada</h3>"
+  str1 <- paste("<p>","<b>Amostra A: </b>",toString(amostra_A),"</p>")
+  str2 <- paste("<p>","<b>Amostra B: </b>",toString(amostra_B),"</p>")
+  HTML(header, str1, str2)
+}
+
+
 ui <- fluidPage(
   
   titlePanel("Estatística - Atividade 1"),
@@ -6,58 +35,36 @@ ui <- fluidPage(
     sidebarPanel(
       numericInput("N", h4("População"), value = "211"),
       numericInput("n", h4("Amostra"), value = "15"),
-      radioButtons("plano", h4("Plano de Amostragem"), 
-                         choices = list("Aleatória Simples" = 1, 
-                                        "Sistemática" = 2, 
-                                        "Estratificada" = 3),
-                         selected = 1),
+      radioButtons(
+        "plano",
+        h4("Plano de Amostragem"),
+        choices = list(
+          "Aleatória Simples" = 1, 
+          "Sistemática" = 2, 
+          "Estratificada" = 3),
+        selected = 1
+        ),
       conditionalPanel(
         condition = "input.plano == 3",
-        numericInput("Nb", h4("Variável"), value = "30"),
+        numericInput("var", h4("Variável"), value = "30"),
       )
     ),
 
     mainPanel(
-      htmlOutput("header"),
-      textOutput("amostra")
+      htmlOutput("amostra")
     )
   )
 )
 
 server <- function(input, output) {
 
-  output$header <- renderText({
+  output$amostra <- renderUI({
     if (input$plano == 1)
-      paste("<h3>Amostragem Aleatória Simples</h3>")
+      simples(input$N, input$n)
     else if (input$plano == 2)
-      paste("<h3>Amostragem Sistemática</h3>")
+      sistematica(input$N, input$n)
     else
-      paste("<h3>Amostragem Estratificada</h3>")
-  })
-
-  output$amostra <- renderText({
-    if (input$plano == 1) {
-      amostra <- sample(input$N, input$n, replace = F)
-      amostra <- sort(amostra)
-      paste(amostra)
-    }
-    else if (input$plano == 2) {
-      k <- round(input$N/input$n)
-      amostra <- sample(1:k,1)
-      amostra <- amostra + (0:(input$n-1))*k
-      amostra <- sort(amostra)
-      paste(amostra)
-    }
-    else {
-      Na <- input$N - input$Nb
-      na <- round((input$n * Na) / input$N)
-      nb <- round((input$n * input$Nb) / input$N)
-      amostra_A <- sample(1:Na, na, replace = F)
-      amostra_B <- sample(Na:input$N, nb, replace = F)
-      amostra_A <- sort(amostra_A)
-      amostra_B <- sort(amostra_B)
-      paste(list(amostra_A), list(amostra_B), sep="\n")
-    }
+      estratificada(input$N, input$n, input$var)
   })
 
 }
